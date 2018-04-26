a8dc698acb7943c638c0efdb1d9966cc12fae08e
amoh-godwin <amohgyebigodwin@gmail.com>
2018-04-26 20:59:25 +0000

    update on search

--- fs.py
+++ fs.py
@@ -20,6 +20,8 @@ class FileSystem():
         super.__self__
         self.Default_LOCATION = "C:/Program Files (x86)/Deuteronomy Works/Peter/Server"
         self._actual_file = ''
+        self._no = 0
+        self._steps = []
         self._file_extension = ''
 
 
@@ -45,34 +47,66 @@ class FileSystem():
         self._getFileName(file)
  
         # this are the steps we'll use and depth we have to go
-        steps = self._actual_file.split('/')
+        self._steps = self._actual_file.split('/')
 
         # some clean ups
-        del steps[0]
+        del self._steps[0]
 
         # the depth of the path
-        depth = len(steps)
+        depth = len(self._steps)
 
         # try to open the file
         try:
 
             # try to find oepn file here
-            no = 1
-            folders = os.listdir(self.Default_LOCATION)
-            for item in folders:
-                if steps[no] == '':
-                    # to return index or dir listing
-                    pass
+            # INITIALISE THE COUNTER
+            self._no = 0
 
-                elif item == steps[no]:
+            if self._steps[self._no] == '':
+                self._no += 1
+                print('blank')
 
-                    # to find out if is a file or directory
-                    if self._is_dir(self.Default_LOCATION + '/' + item):
+            else:
 
-                        # crawl again
+                # call
+                folders = os.listdir(self.Default_LOCATION)
+                if self._steps[self._no] in folders:
+
+                    # if that step is here then we continue
+                    item = self.Default_LOCATION + '/' + self._steps[self._no]
+
+                    if self._is_dir(item):
+                        print('is dir')
+                        
+                        
+                        self._is_blank(item)
+
+                        """# the depth
+                        no += 1
+
+                        # check if the current no is not blank
+                        if steps[no] != '':
+
+                            print('lets crawl')
+                            # crawl again
+                            # needle is second param
+                            self._crawl(item, steps[no])
+
+                        else:
+
+                            # to dir listing or index.html
+                            print('listing')
+                            pass"""
+
+                    else:
+                        # its a file return it
+                        print('is file')
                         pass
 
-                    # its a file return it
+                else:
+
+                    # will couldn't find the file in the nest
+                    print('see the 404')
                     pass
 
             # status code found
@@ -82,3 +116,57 @@ class FileSystem():
 
             # status code not found
             return 404
+
+
+    def _crawl(self, path, needle):
+        folders = os.listdir(path)
+        if needle in folders:
+            
+            # find if is file or dir
+            if self._is_dir(path + '/' + needle):
+                # continue crawling is a dir
+                print('is a dir')
+                self._is_blank(path + '/' + needle)
+
+            else:
+
+                # return, it is a file
+                print('is a file')
+                pass
+            
+        else:
+            
+            # we couldn't find it means we have ended
+            print('another error 404')
+
+    def _is_blank(self, path):
+
+        # depths
+        self._no += 1
+
+        # check if the current no is not blank
+        if self._steps[self._no] != '':
+
+            print('lets crawl')
+            # crawl again
+            # needle is second param
+            self._crawl(path, self._steps[self._no])
+
+        else:
+
+            # to dir listing or index.html
+            print('listing')
+            pass
+
+
+    def _is_dir(self, path):
+        try:
+            os.listdir(path)
+            return True
+        except:
+            return False
+
+
+fs = FileSystem()
+results = FileSystem.search(fs, '/index.html')
+print(results)
