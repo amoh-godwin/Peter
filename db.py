95cf19ef6d88ed6ddc5bb62a366e597f3f44a011
amoh-godwin <amohgyebigodwin@gmail.com>
2020-11-11 16:05:07 +0000

    update db to use id starting with 0

--- db.py
+++ db.py
@@ -3,21 +3,27 @@ import sqlite3
 conn = sqlite3.connect('settings.db')
 cursor = conn.cursor()
 
-sql = """SELECT * FROM Databases"""
-cursor.execute(sql)
-al = cursor.fetchall()
-info = {}
-servers = []
-for server in al:
-    print(server)
-    info['id'] = int(server[0])
-    info['name'] = server[2]
-    info['default_port'] = int(server[4])
-    info['port'] = int(server[5])
-    info['status'] = server[6]
-    servers.append(info)
+def select():
+    sql = """SELECT * FROM Servers"""
+    cursor.execute(sql)
+    al = cursor.fetchall()
+    info = {}
+    servers = []
+    for server in al:
+        print(server)
+        info['id'] = int(server[0])
+        info['name'] = server[2]
+        info['default_port'] = int(server[4])
+        info['port'] = int(server[5])
+        info['status'] = server[6]
+        servers.append(info)
 
-print(info)
-print(servers)
+    print(info)
+    print(servers)
+
+def update():
+    sql = """UPDATE Databases SET id=0"""
+    cursor.execute(sql)
+    conn.commit()
 
 conn.close()
