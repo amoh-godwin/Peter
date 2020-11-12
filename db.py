3e6b751170e90585f247be7d0ace82c5dda7b568
amoh-godwin <amohgyebigodwin@gmail.com>
2020-11-12 17:33:46 +0000

    Update db.py

--- db.py
+++ db.py
@@ -3,13 +3,20 @@ import sqlite3
 conn = sqlite3.connect('settings.db')
 cursor = conn.cursor()
 
+def create_table():
+    sql = """CREATE TABLE database_processes (server_id real, pid real)"""
+    cursor.execute(sql)
+    sql1 = """INSERT INTO database_processes VALUES (0, 0)"""
+    cursor.execute(sql1)
+    conn.commit()
+
 def select():
-    sql = """SELECT * FROM Servers"""
+    sql = """SELECT * FROM database_processes"""
     cursor.execute(sql)
     al = cursor.fetchall()
     info = {}
     servers = []
-    for server in al:
+    """for server in al:
         print(server)
         info['id'] = int(server[0])
         info['name'] = server[2]
@@ -19,7 +26,8 @@ def select():
         servers.append(info)
 
     print(info)
-    print(servers)
+    print(servers)"""
+    print(al)
 
 def update():
     sql = """UPDATE Servers SET upath='C:/Deuteronomy Works/Peter/bin/Peterd'"""
